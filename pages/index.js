60496a8d18645e31ad1bf6b5f5b260d5caa4b1d6
Lucas Pagani <lucas-pagani96@hotmail.com>
2024-06-14 17:59:02 -0300

    feat: add pages folder

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const Home = () =>{
+    return (
+        <Fragment>
+            <h1>Jesus! Eu nem sei o que ele escreveu</h1>
+            <h2>Aqui acabou!</h2>
+        </Fragment>
+    )
+}
+
+export default Home;
